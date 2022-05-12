c270c92111230784379ecce29f54dc6154b5ca80
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-05-12 13:23:13 -0500

    starting code for truncheons.js

---
+++ src/scripts/Truncheons.js
@@ -0,0 +1,17 @@
+
+export const Truncheons = () => {
+    return `
+    <h1>Truncheons & Flagons</h1>
+
+        <section class="teams">
+            <h4>Teams</h2>
+        </section>
+        <section class="players">
+            <h4>Players</h2>
+        </section>
+        <section class="scores">
+            <h4>Total Scores</h2>
+        </section>
+
+    <button class="button" id="submitButton">Submit</button>`
+}
