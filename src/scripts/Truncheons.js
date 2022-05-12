6aee793c5c5f0e1ffd519476bbf28487c85f0574
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-05-12 13:24:37 -0500

    fixed tags

--- src/scripts/Truncheons.js
+++ src/scripts/Truncheons.js
@@ -4,13 +4,13 @@ export const Truncheons = () => {
     <h1>Truncheons & Flagons</h1>
 
         <section class="teams">
-            <h4>Teams</h2>
+            <h3>Teams</h3>
         </section>
         <section class="players">
-            <h4>Players</h2>
+            <h3>Players</h3>
         </section>
         <section class="scores">
-            <h4>Total Scores</h2>
+            <h3>Total Scores</h3>
         </section>
 
     <button class="button" id="submitButton">Submit</button>`
