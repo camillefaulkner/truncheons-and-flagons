ebc3c60e783bf122c6a95c966a73c9859be71e88
Camille Faulkner <camillecfaulkner@gmail.com>
2022-05-12 13:24:21 -0500

    set up main.js

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+import { Truncheons } from "./Truncheons.js"
+import { fetchPlayers, fetchTeams, fetchTeamScores } from "./dataAccess.js"
+
+const mainContainer = document.querySelector(".container")
+
+mainContainer.addEventListener(
+    "stateChanged",
+    customEvent => {
+        render()
+    }
+)
+
+
+
+const render = () => {
+    fetchTeams()
+        .then(() => fetchPlayers())
+        .then(() => fetchTeamScores())
+        .then(
+            () => {
+                mainContainer.innerHTML = Truncheons()
+            }
+        )
+}
+
+render()
