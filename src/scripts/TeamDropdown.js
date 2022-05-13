94f58810b8780a3cda97840f376be6822608459c
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-05-13 09:39:50 -0500

    fixed dropdown list

--- src/scripts/TeamDropdown.js
+++ src/scripts/TeamDropdown.js
@@ -2,12 +2,12 @@ import { getTeams } from "./dataAccess.js"
 
 export const TeamsDropdown = () => {
     let teams = getTeams()
-    let html = `<select>`
+    let html = `<select>
+    <option value="0">Choose one...</option>`
     let teamList = teams.map(team => {
         return `<option value="${team.id}">${team.name}</option>`
     })
     html += teamList.join('')
     html += `</select><br>`
-    html += `<button class="button" id="addplayer">Add Player to Team</button>`
     return html
 }
